4c40b32921aad3d0dd993d1122ac1b0befde6d1f
rootlolID <52448400+rootlolID@users.noreply.github.com>
2022-02-03 18:19:15 +0700

    Update script.js

--- script.js
+++ script.js
@@ -4,29 +4,3 @@ const nav = document.querySelector('nav ul');
 menuToggle.addEventListener('click', function () {
   nav.classList.toggle('slide');
 });
-
-      const scriptURL = 'https://script.google.com/macros/s/AKfycbxCso-_F7nRJ3wcIg40hgRzI1ixxqOiSWEvd_gOx4DVF7SbpT3bcJSPBNPL28VXldrKiQ/exec';
-      const form = document.forms['contact-form'];
-      const btnKirim = document.querySelector('.btn-kirim');
-      const btnLoading = document.querySelector('.btn-loading');
-      const myAlert = document.querySelector('.my-alert');
-
-      form.addEventListener('submit', (e) => {
-        e.preventDefault();
-        // ketika tombol submit di klik
-        // tampilkan tombol loading, tampilkan tombol kirim
-        btnLoading.classList.toggle('d-none');
-        btnKirim.classList.toggle('d-none');
-        fetch(scriptURL, { method: 'POST', body: new FormData(form) })
-          .then((response) => {
-            // tampilkan tombol kirim, tampilkan tombol loading
-            btnLoading.classList.toggle('d-none');
-            btnKirim.classList.toggle('d-none');
-            // tampilkan alert
-            myAlert.classList.toggle('d-none');
-            // reset formnya
-            form.reset();
-            console.log('Success!', response)
-          })
-          .catch((error) => console.error('Error!', error.message));
-      });
